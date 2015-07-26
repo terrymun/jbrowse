5a585976ca9ec858cda5ffe16e2c54d43716930a
Colin <colin.diesh@gmail.com>
2015-07-26 15:51:37 -0400

    Update Gulpfile without uglify

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,12 +5,15 @@ var gulp_concat = require('gulp-concat');
 
 gulp.task('uglify', function() {
   return gulp.src([
-    'src/*.js', 'src/**/*.js', 'src/**/**/*.js',
+    'src/**/*.js','src/**/**/*.js',
+    '*.css','src/**/*.css','src/**/**/*.css',
+    '*.html','src/**/*.html','src/**/**/*.html'
+    '*.conf','*.json'
   ])
-  .pipe(gulp_concat('concat.js'))
-  .pipe(gulp.dest('dist'))
-  .pipe(gulp_rename('uglify.js'))
-  .pipe(gulp_uglify()).on('error', errorHandler)
+  //.pipe(gulp_concat('concat.js'))
+  //.pipe(gulp.dest('dist'))
+  //.pipe(gulp_rename('uglify.js'))
+  //.pipe(gulp_uglify({ mangle: false })).on('error', errorHandler)
   .pipe(gulp.dest('dist'))
 });
 
