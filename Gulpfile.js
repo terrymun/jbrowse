b658337ddc4e2751d978dc19b2437eff2f13f068
Colin <colin.diesh@gmail.com>
2015-07-27 13:10:01 -0500

    Updated Gulpfile to output to separate paths

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,24 +3,43 @@ var gulp_uglify = require('gulp-uglify');
 var gulp_rename = require('gulp-rename');
 var gulp_concat = require('gulp-concat');
 
-gulp.task('uglify', function() {
-  return gulp.src([
+gulp.task('build', function() {
+  gulp.src([
+    '*.json','*.conf','*.html','*.css'
+  ]).pipe(gulp.dest('dist/'))
+
+  gulp.src([
+    'img/*.png'
+  ]).pipe(gulp.dest('dist/img/'))
+
+  gulp.src([
     'src/**/*.js','src/**/**/*.js',
-    '*.css','src/**/*.css','src/**/**/*.css',
-    '*.html','src/**/*.html','src/**/**/*.html',
-    '*.png','src/**/*.png','src/**/**/*.png',
-    '*.gif','src/**/*.gif','src/**/**/*.gif',
-    '*.jpg','src/**/*.jpg','src/**/**/*.jpg',
-    '*.conf','*.json',
+    'src/**/*.css','src/**/**/*.css',
+    'src/**/*.html','src/**/**/*.html',
+    'src/**/*.png','src/**/**/*.png',
+    'src/**/*.gif','src/**/**/*.gif',
+    'src/**/*.jpg','src/**/**/*.jpg',
   ])
+  .pipe(gulp.dest('dist/src/'))
+
+
+  gulp.src([
+    'plugins/**/js/*.js','plugins/**/js/**/*.js',
+    'plugins/**/css/*.css','plugins/**/css/**/*.css',
+  ])
+  .pipe(gulp.dest('dist/plugins/'))
+
+
   //.pipe(gulp_concat('concat.js'))
   //.pipe(gulp.dest('dist'))
   //.pipe(gulp_rename('uglify.js'))
   //.pipe(gulp_uglify({ mangle: false })).on('error', errorHandler)
-  .pipe(gulp.dest('dist'))
+  //.pipe(gulp.dest('dist'))
+
+  return 1
 });
 
-gulp.task('default', ['uglify']);
+gulp.task('default', ['build']);
 
 
 // Handle the error
